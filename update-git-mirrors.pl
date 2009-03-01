1098f03a346e01eb904f79d90ec4e59589828eab
awwaiid <awwaiid@thelackthereof.org>
2009-02-28 20:32:33 -0500

    Add documentation and darcs_git to github
    darcs-hash:20090301013233-4fc8e-1e0b1b6bc2822fd76daea74166e42289748c8c9a.gz

--- update-git-mirrors.pl
+++ update-git-mirrors.pl
@@ -3,6 +3,14 @@
 use strict;
 chdir '/home/awwaiid/tlt/darcs-git';
 
+# To add another, for example "darcs_git" first add it to github, which will
+# give you the remote add command. Switch "origin" to "github"... then do:
+#
+#   cd perl-darcs_git
+#   git remote add github git@github.com:awwaiid/update-darcs-to-git-mirror.git
+#
+# and then add it to the list below!
+
 # Hard-wired list of things mirrored on github
 my %github = (
   'perl-Continuity' => 1,
@@ -10,7 +18,9 @@ my %github = (
   'perl-WWW-HtmlUnit' => 1,
   'perl-Continuity-Monitor' => 1,
   'javascript-autoindent', => 1,
+  'perl-darcs_git' => 1,
 );
+
 
 my @darcs_dirs = `find /home/awwaiid/projects/ -name '_darcs'`;
 @darcs_dirs = map { chomp; s/\/_darcs$// ; tr/'//; $_ } @darcs_dirs;
